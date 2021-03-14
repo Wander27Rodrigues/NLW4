aa5f32abef47dbab41d6f90a5ef3b11f7fec8386
Alisson Rodrigues <48796830+Wander27Rodrigues@users.noreply.github.com>
2021-03-14 20:09:14 -0300

    login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from '../styles/Login.module.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Login = () => (
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+    <form>
+    <label>
+      <p>Username</p>
+      <input type="text" />
+    </label>
+    <label>
+      <p>Password</p>
+      <input type="password" />
+    </label>
     <div>
-       
+      <button type="submit">Submit</button>
     </div>
-  );
+  </form>
+  </div>
+); 
   
   export default Login;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 function MyApp({ Component, pageProps }) {
